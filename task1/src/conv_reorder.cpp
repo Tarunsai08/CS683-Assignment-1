0a605fd4d236e419d7fa0c22c8bdc2edb15cf2a9
Tarunsai <dtarunsai08@gmail.com>
2026-08-22 00:04:05 +0530

    Loop Reordering 1.0

--- task1/src/conv_reorder.cpp
+++ task1/src/conv_reorder.cpp
@@ -6,5 +6,21 @@
 void conv_reorder(const float* in, float* out, const float* ker,
                   int H, int W, int K) {
     // TODO(student): replace this placeholder with your reordered implementation.
-    conv_naive(in, out, ker, H, W, K);
+    const int p = K / 2;
+    const int in_stride = W + 2 * p;
+
+    for(int ky = 0; ky < K; ++ky) {
+        for(int oy = ky; oy < H + ky; oy++) {
+            for(int ox = 0; ox < W; ox++) {
+                float acc = 0.0f;
+                for(int kx = 0; kx < K; kx++) {
+                    acc += in[oy * in_stride + (ox + kx)] * ker[ky * K + kx];
+                }
+                if (ky == 0)
+                    out[(oy - ky) * W + ox] = acc;
+                else
+                    out[(oy - ky) * W + ox] += acc;
+            }
+        }
+    }
 }
