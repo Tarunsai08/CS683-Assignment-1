d756638c0c3755a611759e83344acdcf2c6634cc
Ayush Kumar <ayushkk656@gmail.com>
2026-08-30 02:23:00 +0530

    Worked on conv_tile version 1.0, but not seen any improvement in speedup

--- task1/src/conv_tile.cpp
+++ task1/src/conv_tile.cpp
@@ -1,9 +1,34 @@
 // conv_tile.cpp  STAGE 3: CACHE TILING
 
+// conv_tile version 1.0 
+
 #include "convolution.h"
 
 void conv_tile(const float* in, float* out, const float* ker,
                int H, int W, int K) {
-    // TODO(student): replace this placeholder with your tiled/blocked implementation.
-    conv_naive(in, out, ker, H, W, K);
+    const int TILE = 32;
+    const int p = K / 2;
+    const int in_stride = W + 2 * p;
+
+    for (int ii = 0; ii < H; ii += TILE) {
+        for (int jj = 0; jj < W; jj += TILE) {
+
+            int i_end = (ii + TILE) < H ? (ii + TILE) : H;
+            int j_end = (jj + TILE) < W ? (jj + TILE) : W;
+
+            for (int oy = ii; oy < i_end; oy++) {
+                for (int ox = jj; ox < j_end; ox++) {
+
+                    float acc = 0.0f;
+                    for (int ky = 0; ky < K; ky++) {
+                        for (int kx = 0; kx < K; kx++) {
+                            acc += in[(oy + ky) * in_stride + (ox + kx)] *
+                                   ker[ky * K + kx];
+                        }
+                    }
+                    out[oy * W + ox] = acc;
+                }
+            }
+        }
+    }
 }
