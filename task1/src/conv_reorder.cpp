52636035c3ba9cd396a34b7a63b5304ca2a26a12
Tarun sai <dtarunsai08@gmail.com>
2026-09-06 07:29:03 +0530

    Reordering Version 2.0

--- task1/src/conv_reorder.cpp
+++ task1/src/conv_reorder.cpp
@@ -9,17 +9,34 @@ void conv_reorder(const float* in, float* out, const float* ker,
     const int p = K / 2;
     const int in_stride = W + 2 * p;
 
-    for(int ky = 0; ky < K; ++ky) {
-        for(int oy = ky; oy < H + ky; oy++) {
-            for(int ox = 0; ox < W; ox++) {
-                float acc = 0.0f;
-                for(int kx = 0; kx < K; kx++) {
-                    acc += in[oy * in_stride + (ox + kx)] * ker[ky * K + kx];
+    for (int oy = 0; oy < H; ++oy) {
+        float* out_row = out + oy * W;
+
+        for (int ky = 0; ky < K; ++ky) {
+            const float* in_row = in + (oy + ky) * in_stride;
+            const float* krow = ker + ky * K;
+
+            for (int kx = 0; kx < K; ++kx) {
+                const float w = krow[kx];
+                const float* in_ptr = in_row + kx;
+
+                if (ky == 0 && kx == 0) {
+                    float* out_ptr = out_row;
+
+                    for (int ox = 0; ox < W; ++ox) {
+                        *out_ptr = *in_ptr * w;
+                        ++out_ptr;
+                        ++in_ptr;
+                    }
+                } else {
+                    float* out_ptr = out_row;
+
+                    for (int ox = 0; ox < W; ++ox) {
+                        *out_ptr += *in_ptr * w;
+                        ++out_ptr;
+                        ++in_ptr;
+                    }
                 }
-                if (ky == 0)
-                    out[(oy - ky) * W + ox] = acc;
-                else
-                    out[(oy - ky) * W + ox] += acc;
             }
         }
     }
